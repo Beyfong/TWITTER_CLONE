b4086d4a7cb874a515ca6c323adb165c52ca802b
AMBAL-DAMBAL <ablai2602@gmail.com>
2020-08-07 16:55:49 +0600

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by ablai erzhanov on 8/7/20.
+//  Copyright © 2020 ablai erzhanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
